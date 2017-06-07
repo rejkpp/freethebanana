e087430d17bdb8c6d3ec62579cd8130b3950f9b6
rejkpp <rejkpp@users.noreply.github.com>
2017-06-07 02:17:27 +0200

    generate random number pairs.

--- ass/js/match-game.js
+++ ass/js/match-game.js
@@ -9,9 +9,22 @@ var MatchGame = {};
   Generates and returns an array of matching card values.
  */
 
-MatchGame.generateCardValues = function () {
+ MatchGame.generateCardValues = function () {
+   var numberPairs = [];
 
-};
+   for (var c = 1 ; c < 9 ; c++ ) {
+     numberPairs.push(c,c);
+   }
+
+   var cardValues =[];
+
+   while(numberPairs.length > 0) {
+     var randomIndex = Math.floor(Math.random() * numberPairs.length);
+     var randomNumber= numberPairs.splice(randomIndex,1)[0];
+     cardValues.push(randomNumber);
+   }
+   return cardValues;
+ };
 
 /*
   Converts card values to jQuery card objects and adds them to the supplied game
