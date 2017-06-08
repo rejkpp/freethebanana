1b7ece88f4127a3b917fd23057e9018b3d42a2fa
rejkpp <rejkpp@users.noreply.github.com>
2017-06-08 17:47:25 +0200

    change to 3 columns.

--- ass/js/match-game.js
+++ ass/js/match-game.js
@@ -52,7 +52,7 @@ MatchGame.renderCards = function(numbers, $id)
 
   for ( var n = 0 ; n < numbers.length ; n++ )
   {
-      var $newCard=$('<div class="col-xs-6 card"></div>');
+      var $newCard=$('<div class="col-xs-4 card"></div>');
       $newCard.data('value',numbers[n]);
       $newCard.data('flipped',false);
       $newCard.data('color',colorValues[numbers[n]-1]);
